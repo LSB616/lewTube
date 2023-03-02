5b6c79eb01c5a9cf592d959c0b919559688b30f3
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-03-02 17:07:22 +0000

    small style adjustments

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,13 +5,13 @@ const Videos = ({ videos, direction }) => {
   if(!videos?.length) return 'Loading...';
 
   return (
-    <Stack direction={ direction || "row" } flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack direction={ direction || "row" } flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
         {videos.map((item, index) => (
             <Box key={index}>
                 {item.id.videoId && <VideoCard video={item} />}
                 {item.id.channelId && <ChannelCard channelDetail={item} />}
             </Box>
-        ))}
+  ))}
     </Stack>
   )
 }
